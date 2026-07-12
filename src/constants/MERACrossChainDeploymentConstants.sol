1b92c4f1ab74dbf591a73ce808e4577598546d8f
dyakart <dyakovich.artyom@yandex.ru>
2026-07-12 16:49:39 +0300

    chore: bump deterministic deployment salts to v2

--- src/constants/MERACrossChainDeploymentConstants.sol
+++ src/constants/MERACrossChainDeploymentConstants.sol
@@ -5,8 +5,8 @@ pragma solidity 0.8.34;
 /// @notice Versioned salts for deterministic WalletMera deployments.
 library MERACrossChainDeploymentConstants {
     bytes32 internal constant CROSS_CHAIN_DEPLOYER_SALT = keccak256("WalletMera.CrossChainDeployer.v1");
-    bytes32 internal constant WALLET_IMPLEMENTATION_SALT = keccak256("WalletMera.BaseMERAWallet.v1");
-    bytes32 internal constant LOGIN_REGISTRY_SALT = keccak256("WalletMera.LoginRegistry.v1");
-    bytes32 internal constant LOGIN_VERIFIER_SALT = keccak256("WalletMera.LoginSignatureVerifier.v1");
-    bytes32 internal constant WALLET_FACTORY_SALT = keccak256("WalletMera.MetaProxyCloneFactory.v1");
+    bytes32 internal constant WALLET_IMPLEMENTATION_SALT = keccak256("WalletMera.BaseMERAWallet.v2");
+    bytes32 internal constant LOGIN_REGISTRY_SALT = keccak256("WalletMera.LoginRegistry.v2");
+    bytes32 internal constant LOGIN_VERIFIER_SALT = keccak256("WalletMera.LoginSignatureVerifier.v2");
+    bytes32 internal constant WALLET_FACTORY_SALT = keccak256("WalletMera.MetaProxyCloneFactory.v2");
 }
